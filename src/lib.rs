1952c3ad5739dd8fb87aa6c0bc221bec5a46ce3e
wcampbell <wcampbell1995@gmail.com>
2022-04-05 08:38:11 -0400

    add tests

--- src/lib.rs
+++ src/lib.rs
@@ -26,3 +26,41 @@ pub fn decompress(data_len: usize, bits_set: u32, hash: &str) -> Option<Vec<u8>>
 
     None
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn test(bytes: &[u8], hash: &str) {
+        let total_bits = total_bits(bytes);
+        assert_eq!(Some(bytes.to_vec()), decompress(bytes.len(), total_bits, hash));
+    }
+
+    #[test]
+    fn test_01() {
+        let bytes = &[0xa; 1];
+        let hash = "01ba4719c80b6fe911b091a7c05124b64eeece964e09c058ef8f9805daca546b";
+        test(bytes, hash);
+    }
+
+    #[test]
+    fn test_02() {
+        let bytes = &[0xa; 2];
+        let hash = "75a11da44c802486bc6f65640aa48a730f0f684c5c07a42ba3cd1735eb3fb070";
+        test(bytes, hash);
+    }
+
+    #[test]
+    fn test_03() {
+        let bytes = &[0xa; 3];
+        let hash = "6a3cf5192354f71615ac51034b3e97c20eda99643fcaf5bbe6d41ad59bd12167";
+        test(bytes, hash);
+    }
+
+    #[test]
+    fn test_04() {
+        let bytes = &[0xa; 4];
+        let hash = "545c38b0922de19734fbffde62792c37c2aef6a3216cfa472449173165220f7d";
+        test(bytes, hash);
+    }
+}
