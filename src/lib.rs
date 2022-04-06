921bc9f580f0d9f8a1ca2a3d31e3b232879c51e7
wcampbell <wcampbell1995@gmail.com>
2022-04-05 23:25:01 -0400

    Refactor to generic hashing
    Refactor decompress to support any hashing algorithm implementing the
    digest trait.
    
    For now, this just uses the sh256 type for binary, test, and benches.

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,11 @@
+use digest::Digest;
 use itertools::Itertools;
 
 pub fn total_bits(data: &[u8]) -> u32 {
     data.iter().map(|a| a.count_ones()).sum()
 }
 
-pub fn decompress(data_len: usize, bits_set: u32, hash: &str) -> Option<Vec<u8>> {
+pub fn decompress<D: Digest>(data_len: usize, bits_set: u32, hash: &str) -> Option<Vec<u8>> {
     for combinations in (0..(data_len as usize * 8))
         .collect::<Vec<usize>>()
         .iter()
@@ -17,8 +18,10 @@ pub fn decompress(data_len: usize, bits_set: u32, hash: &str) -> Option<Vec<u8>>
                 a[*bit / 8] |= 1 << (bit % 8);
             }
 
-            let sus_hash = sha256::digest_bytes(&a);
-            if sus_hash == hash {
+            let mut hasher = D::new();
+            hasher.update(&a);
+            let sus_hash = hasher.finalize();
+            if hash == base16ct::lower::encode_string(&sus_hash) {
                 return Some(a);
             }
         }
@@ -30,37 +33,41 @@ pub fn decompress(data_len: usize, bits_set: u32, hash: &str) -> Option<Vec<u8>>
 #[cfg(test)]
 mod tests {
     use super::*;
+    use sha2::Sha256;
 
-    fn test(bytes: &[u8], hash: &str) {
+    fn test_256(bytes: &[u8], hash: &str) {
         let total_bits = total_bits(bytes);
-        assert_eq!(Some(bytes.to_vec()), decompress(bytes.len(), total_bits, hash));
+        assert_eq!(
+            Some(bytes.to_vec()),
+            decompress::<Sha256>(bytes.len(), total_bits, hash)
+        );
     }
 
     #[test]
     fn test_01() {
         let bytes = &[0xa; 1];
         let hash = "01ba4719c80b6fe911b091a7c05124b64eeece964e09c058ef8f9805daca546b";
-        test(bytes, hash);
+        test_256(bytes, hash);
     }
 
     #[test]
     fn test_02() {
         let bytes = &[0xa; 2];
         let hash = "75a11da44c802486bc6f65640aa48a730f0f684c5c07a42ba3cd1735eb3fb070";
-        test(bytes, hash);
+        test_256(bytes, hash);
     }
 
     #[test]
     fn test_03() {
         let bytes = &[0xa; 3];
         let hash = "6a3cf5192354f71615ac51034b3e97c20eda99643fcaf5bbe6d41ad59bd12167";
-        test(bytes, hash);
+        test_256(bytes, hash);
     }
 
     #[test]
     fn test_04() {
         let bytes = &[0xa; 4];
         let hash = "545c38b0922de19734fbffde62792c37c2aef6a3216cfa472449173165220f7d";
-        test(bytes, hash);
+        test_256(bytes, hash);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,22 @@
+use sha2::{Digest, Sha256};
+
 fn main() {
-    let data = &[0xa; 6];
-    let orig_hash = sha256::digest_bytes(data);
+    // sample input
+    let data = &[0xa; 4];
+
+    // example usage of creating a sha256 hash
+    let mut hasher = sha2::Sha256::new();
+    hasher.update(&data);
+    let sus_hash = hasher.finalize();
+    let orig_hash = base16ct::lower::encode_string(&sus_hash);
     println!("{data:x?}");
     println!("{orig_hash:x?}");
+
+    // saving the amount of total bits
     let total_bits = sorensen::total_bits(data);
-    let uncompressed_data = sorensen::decompress(data.len(), total_bits, &orig_hash);
+
+    // decompress data with sha256
+    let uncompressed_data = sorensen::decompress::<Sha256>(data.len(), total_bits, &orig_hash);
 
     println!("recovered: {uncompressed_data:x?}");
 }
